eb241fb4d0d9b9f22f880b2d0b71a37f4729b472
Alix Levé <alix.leve@gmail.com>
2024-12-19 16:24:38 +0100

    add: tailwind

--- src/app/List.jsx
+++ src/app/List.jsx
@@ -2,5 +2,17 @@
 
 export default function List({parisBookshops}) {
   console.log(parisBookshops);
-  return <p>COUCOU</p>;
+  return <>
+  <div className="flex flex-col gap-10"> 
+  {parisBookshops.map((bookshop) => (
+    <div>
+    <h4>{bookshop.fields.nom_structure}</h4>
+    <p>{bookshop.fields.adresse}</p>
+    <p>{bookshop.fields.code_postal}</p>
+    <p>{bookshop.fields.ville}</p>
+    </div>
+
+  ))}
+  </div>
+  </>
 }
