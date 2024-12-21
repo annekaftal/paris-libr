c2cb51bfaefe512798ecf59042497241a73ee89f
annekaftal <anne.kaftal@gmail.com>
2024-12-21 17:29:43 +0100

    Fix: fly to districts

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -18,6 +18,22 @@ export default function Map({ parisBookshops }) {
     [48.86898506731983, 2.342585252549832],
     [48.864981225451785, 2.3625041301806617],
     [48.855326750747096, 2.3609562677659044],
+    [48.84330633875683, 2.3536252592422473],
+    [48.85136566100697, 2.335051272854635],
+    [48.85714776928274, 2.3125991864349533],
+    [48.87354645913075, 2.313759208468015],
+    [48.87663775335831, 2.340080271697847],
+    [48.876826906212465, 2.3581972247334386],
+    [48.858455888071376, 2.3793064533715547],
+    [48.843141694359055, 2.3896860468534222],
+    [48.82986623338986, 2.363082744482322],
+    [48.83230446903985, 2.3264298862716855],
+    [48.84203207217297, 2.297321651456523],
+    [48.86212268166178, 2.282547310670731],
+    [48.88706767795717, 2.3110217810106706],
+    [48.890467526110015, 2.3494228894104956],
+    [48.88653029924089, 2.3847624972884724],
+    [48.863936141695014, 2.403323017682067],
   ];
 
   const [district, setDistrict] = useState("null");
@@ -30,11 +46,10 @@ export default function Map({ parisBookshops }) {
     const map = useMap();
     const flyToMarker = useCallback(
       (coordinates) => {
-        if (coordinates[district])
-          map.flyTo(coordinates, 15, {
-            animation: true,
-            duration: 1.5,
-          });
+        map.flyTo(coordinates, 15, {
+          animation: true,
+          duration: 1.5,
+        });
       },
       [map]
     );
@@ -78,6 +93,22 @@ export default function Map({ parisBookshops }) {
         <option value="1">Paris 2e</option>
         <option value="2">Paris 3e</option>
         <option value="3">Paris 4e</option>
+        <option value="4">Paris 5e</option>
+        <option value="5">Paris 6e</option>
+        <option value="6">Paris 7e</option>
+        <option value="7">Paris 8e</option>
+        <option value="8">Paris 9e</option>
+        <option value="9">Paris 10e</option>
+        <option value="10">Paris 11e</option>
+        <option value="11">Paris 12e</option>
+        <option value="12">Paris 13e</option>
+        <option value="13">Paris 14e</option>
+        <option value="14">Paris 15e</option>
+        <option value="15">Paris 16e</option>
+        <option value="16">Paris 17e</option>
+        <option value="17">Paris 18e</option>
+        <option value="18">Paris 19e</option>
+        <option value="19">Paris 20e</option>
       </select>
     </>
   );
