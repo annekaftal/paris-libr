fbdacb3b90bc04e62e8c00813393aa5926c1b904
annekaftal <anne.kaftal@gmail.com>
2024-12-21 17:14:58 +0100

    Add: fly to district first version

--- components/Map/Map.js
+++ components/Map/Map.js
@@ -13,17 +13,25 @@ import "../../src/app/globals.css";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Map({ parisBookshops }) {
-  const [district, setDistrict] = useState(null);
+  const coordinates = [
+    [48.863704964791864, 2.334678158416777],
+    [48.86898506731983, 2.342585252549832],
+    [48.864981225451785, 2.3625041301806617],
+    [48.855326750747096, 2.3609562677659044],
+  ];
+
+  const [district, setDistrict] = useState("null");
   console.log(district);
+  console.log(coordinates[district]);
 
   const mapRef = useRef();
 
   const ZoomHandler = () => {
     const map = useMap();
     const flyToMarker = useCallback(
-      (coordinates, zoom) => {
-        if (district !== "")
-          map.flyTo(coordinates, zoom, {
+      (coordinates) => {
+        if (coordinates[district])
+          map.flyTo(coordinates, 15, {
             animation: true,
             duration: 1.5,
           });
@@ -31,16 +39,18 @@ export default function Map({ parisBookshops }) {
       [map]
     );
     useEffect(() => {
-      if (district) {
-        if (district !== "") {
-          flyToMarker([48.960213, 2.042567], 10);
-        }
+      if (coordinates[district]) {
+        flyToMarker(coordinates[district]);
       }
     }, [flyToMarker]);
   };
 
-  const handleClickTwenty = () => {
-    setDistrict("75020");
+  //   const handleClick = () => {
+  //     setDistrict("75020");
+  //   };
+  const handleSelect = (e) => {
+    const selectedDistrict = e.target.value;
+    setDistrict(selectedDistrict);
   };
   return (
     <>
@@ -61,7 +71,14 @@ export default function Map({ parisBookshops }) {
         ))}
         <ZoomHandler />
       </MapContainer>
-      <button onClick={handleClickTwenty}>Paris 20e</button>
+      {/* <button onClick={handleClick}>Paris 20e</button> */}
+      <select onChange={(e) => handleSelect(e)}>
+        <option>Select</option>
+        <option value="0">Paris 1e</option>
+        <option value="1">Paris 2e</option>
+        <option value="2">Paris 3e</option>
+        <option value="3">Paris 4e</option>
+      </select>
     </>
   );
 }
